6eb3f7e70d1db7f58087b1ce8a0a39771cbf5e36
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-24 10:49:58 -0800

    Added webhooks and branch protection

--- modules/repositories/main.tf
+++ modules/repositories/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "integrations/github"
       version = "~> 4"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3"
+    }
   }
   required_version = "~> 1"
 }
@@ -22,7 +26,41 @@ locals {
       vulnerability_alerts   = true
     }
   })
+  repository_hooks = { for hook in flatten([
+    for _, workflow in var.workflows_info : [
+      for _, repo in github_repository.main : {
+        name          = workflow.name
+        repo          = repo.name
+        webhook_token = workflow.webhook_token
+        webhook_url   = workflow.webhook_url
+      } if contains(repo.topics == null ? [] : repo.topics, workflow.name)
+    ]
+    ]) : "${hook.repo}:${hook.name}" => hook
+  }
 }
+
+resource "github_branch_protection" "main" {
+  for_each = { for repo in github_repository.main : repo.name => repo if repo.visibility == "public" }
+
+  allows_deletions       = false
+  allows_force_pushes    = false
+  enforce_admins         = true
+  pattern                = "main"
+  push_restrictions      = []
+  repository_id          = each.value.name
+  require_signed_commits = true
+
+  # required_pull_request_reviews {
+  #   dismissal_restrictions = []
+  #   dismiss_stale_reviews  = true
+  # }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
+}
+
 resource "github_repository" "main" {
   for_each = local.conf.repositories
 
@@ -38,4 +76,33 @@ resource "github_repository" "main" {
     ignore_changes  = [etag]
     prevent_destroy = true
   }
+}
+
+resource "github_repository_webhook" "main" {
+  # for each workflow filter repositories with that tag
+  for_each = local.repository_hooks
+
+  active     = true
+  events     = ["push"]
+  repository = each.value.repo
+
+  configuration {
+    url          = each.value.webhook_url
+    content_type = "json"
+    secret       = each.value.webhook_token
+    insecure_ssl = false
+  }
+}
+
+resource "github_user_gpg_key" "main" {
+  count = local.conf.gpg != null ? 1 : 0
+
+  armored_public_key = base64decode(local.conf.gpg.public_key_base64)
+}
+
+resource "github_user_ssh_key" "main" {
+  count = local.conf.ssh != null ? 1 : 0
+
+  title = local.conf.ssh.title
+  key   = base64decode(local.conf.ssh.public_key_base64)
 }
--- modules/repositories/variables.tf
+++ modules/repositories/variables.tf
@@ -1,5 +1,8 @@
 variable "conf" {
   type = object({
+    gpg = optional(object({
+      public_key_base64 = string
+    }))
     owner = optional(string)
     repositories = map(object({
       delete_branch_on_merge = optional(string)
@@ -9,5 +12,17 @@ variable "conf" {
       visibility             = optional(string)
       vulnerability_alerts   = optional(string)
     }))
+    ssh = optional(object({
+      public_key_base64 = string
+      title             = string
+    }))
   })
+}
+
+variable "workflows_info" {
+  type = map(object({
+    name          = string
+    webhook_token = string
+    webhook_url   = string
+  }))
 }
